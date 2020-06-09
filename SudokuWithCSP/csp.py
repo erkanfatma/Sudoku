7614e57ebdcd3996a79d46ba8a1db01759f2373e
Fatma Erkan <erkann.fatma@gmail.com>
2020-06-09 03:04:11 +0300

    updated.

--- SudokuWithCSP/csp.py
+++ SudokuWithCSP/csp.py
@@ -1,46 +1,49 @@
-# to represent csp
-class CSP:
-    def __init__(self, board):
-        self.board = board
+"""
+Sudoku with Constraint satisfaction problems with Backtracking
+"""
 
-    # finds the next empty variable to be assigned in the board
-    def find_empty(self, board):
-        for row in board:
+class CSP:
+    def __init__(self, puzzleboard):
+        self.puzzleboard = puzzleboard
+
+    # finds empty variable in the puzzleboard
+    def empty(self, puzzleboard):
+        for row in puzzleboard:
             for variable in row:
                 if variable.value == 0:
                     return variable
 
-    # check if the board is solved
-    def complete(self, board):
+    # find if puzzleboard solved or not
+    def complete(self, puzzleboard):
         for i in range(9):
             for j in range(9):
-                if board[i][j].value == 0:
+                if puzzleboard[i][j].value == 0:
                     return False
 
-                if self.complete_helper(board, i, j) == False:
+                if self.complete_helper(puzzleboard, i, j) == False:
                     return False
 
         return True
 
-    #  function to evaluate completeness of a board
-    def complete_helper(self, board, row, col):
-        if len(board) == 0:
+    # evaluate completeness of a puzzleboard
+    def complete_helper(self, puzzleboard, row, col):
+        if len(puzzleboard) == 0:
             return False
 
-        value = board[row][col].value
+        value = puzzleboard[row][col].value
 
         # check vertically
-        for r in range(len(board)):
+        for r in range(len(puzzleboard)):
             if (r == row):
                 continue
-            if (board[r][col].value == value):
+            if (puzzleboard[r][col].value == value):
                 return False
 
         # check horizontally
-        for c in range(len(board[row])):
+        for c in range(len(puzzleboard[row])):
             if (c == col):
                 continue
-            if (board[row][c].value == value):
+            if (puzzleboard[row][c].value == value):
                 return False
 
         # check 3x3 box
@@ -54,22 +57,22 @@ class CSP:
             for c in col_indecies_to_check:
                 if (r == row and c == col):
                     continue
-                if (board[r][c].value == value):
+                if (puzzleboard[r][c].value == value):
                     return False
 
         return True
 
-# to define empty cells and which numbers can be selected for this empty cell
-class Variable:
-    def __init__(self, row, col, value=0):
+# to define empty cells and which numbers can be chosen for selected empty cell
+class Vars:
+    def __init__(self, row, column, value=0):
         self.value = value
         self.row = row
-        self.col = col
-        self.domain = self.init_domain(value)
+        self.col = column
+        self.domain = self.definedomain(value)
 
     # initializes domain of the variable
-    #to find domains of empty cell
-    def init_domain(self, value):
+    def definedomain(self, value):
+        # to find available numbers of empty cell
         domain = []
         for i in range(1, 10):
             #to evaluate domains if value can get these numbers
@@ -77,9 +80,3 @@ class Variable:
                 domain.append(i)
 
         return domain
-
-
-
-
-
-
--- SudokuWithCSP/sudokusolver.py
+++ SudokuWithCSP/sudokusolver.py
@@ -6,133 +6,134 @@ from termcolor import colored
 from csp import *
 import time
 
-# Display the solution of sudoku in the grid format
-def display(board):
+# Display the sudoku in the grid format on the screen
+def display(puzzleboard):
     for r in range(9):
         if r in [0, 3, 6]:
             print("-------------------------------------------")
         for c in range(9):
             if c in [3, 6]:
-                print(' | ', board[r][c].value, ' ', end=' ')
+                print(' | ', puzzleboard[r][c].value, ' ', end=' ')
             else:
-                print(board[r][c].value, ' ', end=' ')
+                print(puzzleboard[r][c].value, ' ', end=' ')
         print(end='\n')
 
 
-# returns a list of initialized variables
-def init_variables():
-    #çözülmek istenen sudoku tanımı
-    lists_var = [[0, 7, 0, 0, 4, 2, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 8, 6, 1, 0],
-                 [3, 9, 0, 0, 0, 0, 0, 0, 7],
-                 [0, 0, 0, 0, 0, 4, 0, 0, 9],
-                 [0, 0, 3, 0, 0, 0, 7, 0, 0],
-                 [5, 0, 0, 1, 0, 0, 0, 0, 0],
-                 [8, 0, 0, 0, 0, 0, 0, 7, 6],
-                 [0, 5, 4, 8, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 6, 1, 0, 0, 5, 0]]
+# returns a list of variables
+def initialize():
+    #initial sudoku
+    sudoku_puzzle = [[0, 7, 0, 0, 4, 2, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 8, 6, 1, 0],
+                     [3, 9, 0, 0, 0, 0, 0, 0, 7],
+                     [0, 0, 0, 0, 0, 4, 0, 0, 9],
+                     [0, 0, 3, 0, 0, 0, 7, 0, 0],
+                     [5, 0, 0, 1, 0, 0, 0, 0, 0],
+                     [8, 0, 0, 0, 0, 0, 0, 7, 6],
+                     [0, 5, 4, 8, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 6, 1, 0, 0, 5, 0]]
 
     for i in range(9):
         for j in range(9):
-            #to create Variable set in CSP
-            var = Variable(i, j, lists_var[i][j])
-            lists_var[i][j] = var
+            #to create Variable set in CSP that is called Vars
+            var = Vars(i, j, sudoku_puzzle[i][j])
+            sudoku_puzzle[i][j] = var
 
-    return lists_var
+    return sudoku_puzzle
 
 
-# Returns True if variable_to_check is valid input into board
-# Checks horizontal, vertical, and box
-# Does NOT actually input variable_to_check into board
-# This still needs to be tested
-def is_valid_input(empty_var, variable_to_check, board):
-    row_loc = empty_var.row
-    col_loc = empty_var.col
+# Returns True if checkvariable is valid input into puzzleboard
+# Does NOT actually input checkvariable into puzzleboard
+def controlinput(empty_var, checkvariable, puzzle):
+    row_location = empty_var.row
+    col_location = empty_var.col
 
     # check horizontally
-    for c in range(len(board[row_loc])):
-        if (c == col_loc):
+    for c in range(len(puzzle[row_location])):
+        if (c == col_location):
             continue
-        if (board[row_loc][c].value == variable_to_check):
+        if (puzzle[row_location][c].value == checkvariable):
             return False
 
     # check vertically
-    for r in range(len(board)):
-        if (r == row_loc):
+    for r in range(len(puzzle)):
+        if (r == row_location):
             continue
-        if (board[r][col_loc].value == variable_to_check):
+        if (puzzle[r][col_location].value == checkvariable):
             return False
 
     # check box
-    box_row = row_loc / 3
-    box_col = col_loc / 3
+    box_row = row_location / 3
+    box_col = col_location / 3
     top_left_row = int(box_row) * 3
     top_left_col = int(box_col) * 3
     row_indecies_to_check = [top_left_row, top_left_row + 1, top_left_row + 2]
     col_indecies_to_check = [top_left_col, top_left_col + 1, top_left_col + 2]
     for r in row_indecies_to_check:
         for c in col_indecies_to_check:
-            if (r == row_loc and c == col_loc):
+            if (r == row_location and c == col_location):
                 continue
-            if (board[r][c].value == variable_to_check):
+            if (puzzle[r][c].value == checkvariable):
                 return False
 
     return True
 
 
 # Recursive Backtracking Solver
-def Back_Tracking_Search(csp):
-    if csp.complete(csp.board):  # if assignment is complete then
-        return True  # return assignment
+def BackTrackingSearch(csp):
+    # check if assignment complete
+    if csp.complete(csp.puzzleboard):
+        # return assignment
+        return True
 
-    empty_var = csp.find_empty(csp.board)
+    empty_var = csp.empty(csp.puzzleboard)
 
     if empty_var == None:
         return False
 
-    for domain_var in empty_var.domain:  # for each value in Order-Domain-Value(variable, assignment, csp) do
-        valid = is_valid_input(empty_var, domain_var, csp.board)
-        if valid:  # if value is consistent with assignment given Constraints[csp] then
-            csp.board[empty_var.row][empty_var.col].value = domain_var  # add {variable = value} to assignment
+    # for each value in Order-Domain-Value(variable, assignment, csp) do
+    for domain_var in empty_var.domain:
+        valid = controlinput(empty_var, domain_var, csp.puzzleboard)
+        # if value is consistent with assignment given Constraints[csp] then
+        if valid:
+            # add {variable = value} to assignment
+            csp.puzzleboard[empty_var.row][empty_var.col].value = domain_var
 
-            # result = Recursive-backtracking(assignment, csp) / if result is not failure then
-            if Back_Tracking_Search(csp):
-                return True  # return result
-            ev = Variable(empty_var.row, empty_var.col, 0)
-            csp.board[empty_var.row][empty_var.col] = ev  # remove {variable = value} from assignment
+            #Recursive-backtracking(assignment, csp)
+            if BackTrackingSearch(csp):
+                return True  # result
+            ev = Vars(empty_var.row, empty_var.col, 0)
+            csp.puzzleboard[empty_var.row][empty_var.col] = ev  # remove {variable = value} from assignment
 
-    return False  # return FAILURE
+    # Failure
+    return False
 
 
 def main():
-    # create a 2D lists of variables
-    board = init_variables()
-    display(board)
+    # create variables
+    puzzleboard = initialize()
+    display(puzzleboard)
 
-    # create CPS instance to represent the board
-    csp = CSP(board)
+    # create CPS instance
+    csp = CSP(puzzleboard)
 
-    # start timer
     starttime = time.time()
 
-    # solution is boolean value. True when there's a solution. False otherwise
-    solution = Back_Tracking_Search(csp)
+    # if solution exist then return true
+    solution = BackTrackingSearch(csp)
 
-    # stop timer
     endtime = time.time()
 
     if solution == True:
         print(colored("Congratulations! Sudoku solved", "blue"))
-        display(csp.board)
+        display(csp.puzzleboard)
 
-        print(colored("\nThe algorithm took {0:0.1f} seconds to find solution"
+        print(colored("\nThe algorithm took {0:0.4f} seconds to find solution"
                       .format(endtime - starttime), "blue"))
 
     else:
         print(colored("Sudoku provided has no solution", "blue"))
-        print(colored("\nThe algorithm took {0:0.1f} seconds to find solution"
+        print(colored("\nThe algorithm took {0:0.4f} seconds to find solution"
                       .format(endtime - starttime), "blue"))
-
 
 if __name__ == "__main__":
     main()
